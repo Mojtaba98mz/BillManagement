bf3766041f73b17261e525dc7ead8d85e66281dc
Mojtaba <mojtaba98.mz@gmail.com>
2025-01-14 11:20:38 +0330

    Initialize Project

---
+++ src/test/java/org/example/billmanagement/BillManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.billmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
