f9579a3c86d5e1ad9668205b57d4cb03bb319e98
Mojtaba <mojtaba98.mz@gmail.com>
2025-01-15 20:37:49 +0330

    feat(user): add create user to the UserService

---
+++ src/main/java/org/example/billmanagement/controller/vm/UserDto.java
@@ -0,0 +1,26 @@
+package org.example.billmanagement.controller.vm;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String username;
+
+    @Size(max = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    private String lastName;
+
+    @NotNull
+    @Size(min = 4, max = 100)
+    private String password;
+
+}
--- src/main/java/org/example/billmanagement/service/UserService.java
+++ src/main/java/org/example/billmanagement/service/UserService.java
@@ -1,7 +1,62 @@
 package org.example.billmanagement.service;
 
+import org.example.billmanagement.controller.vm.UserDto;
+import org.example.billmanagement.model.Role;
+import org.example.billmanagement.model.User;
+import org.example.billmanagement.repository.RoleRepository;
+import org.example.billmanagement.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class UserService {
+
+    private final Logger log = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final RoleRepository roleRepository;
+
+    public UserService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            RoleRepository roleRepository
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+    }
+
+
+    public User createUser(UserDto userDto) {
+        User user = new User();
+        user.setUsername(userDto.getUsername().toLowerCase());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        String encryptedPassword = passwordEncoder.encode(userDto.getPassword());
+        user.setPassword(encryptedPassword);
+        Set<String> defaultAuthorities = new HashSet<>();
+        defaultAuthorities.add("ROLE_USER");
+        Set<Role> authorities = defaultAuthorities
+                .stream()
+                .map(roleRepository::findByName)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toSet());
+        user.setAuthorities(authorities);
+        userRepository.save(user);
+        log.debug("Created Information for User: {}", user);
+        return user;
+    }
 }
