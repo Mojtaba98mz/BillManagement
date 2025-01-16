5a1a7c34beeaaf10ee8311f0eec17f596f3c7d2e
Mojtaba <mojtaba98.mz@gmail.com>
2025-01-16 13:18:49 +0330

    fix(exception): improve GlobalExceptionHandler for BadRequest and BadCredentials

--- src/main/java/org/example/billmanagement/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/billmanagement/controller/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package org.example.billmanagement.controller.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,4 +22,20 @@ public class GlobalExceptionHandler {
         problemDetail.setTitle("Internal Server Error");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problemDetail);
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ProblemDetail> handleWrongUsernamePassword(Exception ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.getMessage());
+        problemDetail.setTitle("Bad credentials");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(problemDetail);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProblemDetail> handleArgumentValidationException(Exception ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Invalid argument");
+        problemDetail.setTitle("Bad Request");
+        problemDetail.setProperty("errors","argument_validation_exception");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+    }
+
 }
