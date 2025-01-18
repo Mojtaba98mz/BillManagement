c01a917aeb6d439810b979d39da41b05a772aa53
Mojtaba <mojtaba98.mz@gmail.com>
2025-01-18 20:36:28 +0330

    fix(bill): add query method to resolve cascade remove

--- src/main/java/org/example/billmanagement/repository/BillRepository.java
+++ src/main/java/org/example/billmanagement/repository/BillRepository.java
@@ -2,7 +2,14 @@ package org.example.billmanagement.repository;
 
 import org.example.billmanagement.model.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BillRepository extends JpaRepository<Bill, Long> {}
+public interface BillRepository extends JpaRepository<Bill, Long> {
+    @Modifying
+    @Query("DELETE FROM Bill b WHERE b.member IN (SELECT m FROM Member m WHERE m.group.id = :groupId)")
+    void deleteByGroupId(@Param("groupId") Long groupId);
+}
